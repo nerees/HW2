d3b73427386ee0f4980cc953fc611ae3ed6e6d69
Nerijus Latakas <32032495+nerees@users.noreply.github.com>
2017-10-15 22:21:37 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+
+//Antra dalis
+require_once 'vendor/autoload.php';
+use Incl\more\groupa\A;
+use Incl\more\groupa\C;
+use Incl\more\groupb\B;
+use Incl\more\groupb\D;
+
+echo '<h2>ANTRA DALIS</h2>';
+PHP_EOL;
+$a = new A(); //gaunam pranesima is a klases
+
+echo '<br>';
+$b = new B(); //gaunam pranesima is b klases
+echo '<br>';
+$c = new C(); //gaunam pranesima is c klases
+echo '<br>';
+$d = new D(); //gaunam pranesima is d klases
+
+
